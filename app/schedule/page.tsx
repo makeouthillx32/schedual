44a619af251937b59e6d7ec429838e29427ece3e
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-10 14:06:09 -0800

    Update page.tsx

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,7 +1,13 @@
-"use client";
-
-import Hero2 from "@/components/hero2";
+import Hero2 from "@/components/hero2"; // Ensure correct path
+import Nav from "@/components/nav";
+import Footer from "@/components/footer";
 
 export default function SchedulePage() {
-  return <Hero2 />;
+  return (
+    <>
+      <Nav />
+      <Hero2 />
+      <Footer />
+    </>
+  );
 }

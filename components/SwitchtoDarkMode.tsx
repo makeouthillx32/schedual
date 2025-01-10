241e35428a2275e03bf17c5a55d807148a87744e
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-10 10:28:54 -0800

    Update SwitchtoDarkMode.tsx

--- components/SwitchtoDarkMode.tsx
+++ components/SwitchtoDarkMode.tsx
@@ -1,29 +1,19 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { useTheme } from "@/app/layout";
+import React from "react";
+import { useTheme } from "@/app/providers";
 
 const SwitchtoDarkMode: React.FC = () => {
   const { themeType, toggleTheme } = useTheme();
-  const [isChecked, setIsChecked] = useState(themeType === "dark");
-
-  useEffect(() => {
-    setIsChecked(themeType === "dark");
-  }, [themeType]);
 
   return (
-    <div className="flex items-center">
-      <label htmlFor="dark-mode-toggle" className="sr-only">
-        Toggle Dark Mode
-      </label>
-      <input
-        id="dark-mode-toggle"
-        className="toggle"
-        type="checkbox"
-        checked={isChecked}
-        onChange={toggleTheme}
-      />
-    </div>
+    <button
+      aria-label="Toggle Dark Mode"
+      onClick={toggleTheme}
+      className="p-2 rounded focus:outline-none"
+    >
+      {themeType === "dark" ? "🌙" : "☀️"}
+    </button>
   );
 };
 

84c685f1da26d62678fde8689bf0489525cf2352
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-10 11:37:15 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import Hero from "@/components/hero";
+import Footer from "@/components/footer";
 
 export default function Page() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Footer />
+    </>
+  );
 }

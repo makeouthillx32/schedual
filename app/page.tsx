1fa741e06ef641700fb65ac5152291969f1a5a3c
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-10 11:40:24 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Hero from "@/components/hero";
+import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 
 export default function Page() {
   return (
     <>
+      <Nav />
       <Hero />
       <Footer />
     </>

bf6858980f41f87a50d5c5058337ced0bfa54964
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-10 13:39:08 -0800

    Update layout.tsx

---
+++ app/schedule/layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Providers } from "./provider"; // Ensure correct path
+import Nav from "@/components/nav";
+import Footer from "@/components/footer";
+import "./globals.css";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          <Nav />
+          <main>{children}</main>
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;

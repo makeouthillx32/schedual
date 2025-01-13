d76aeec99044a3930320b3f2a1b8b62b3ee98f51
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-13 10:02:39 -0800

    Update layout.tsx

--- app/schedule/layout.tsx
+++ app/schedule/layout.tsx
@@ -3,7 +3,7 @@
 import { Providers } from "@/app/provider";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
-import "@/app/globals.css"; // Correct absolute path to your CSS
+import "@/app/globals.css";
 
 interface LayoutProps {
   children: React.ReactNode;

ed29c9ff3a8b9137a00da8d64e55cd9a500a873a
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-14 12:00:10 -0800

    Update SwitchtoDarkMode.tsx

--- components/SwitchtoDarkMode.tsx
+++ components/SwitchtoDarkMode.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import React from "react";
-import { useTheme } from "@/app/provider";
 
 interface SwitchtoDarkModeProps {
   toggleTheme: () => void;
 }
 
 const SwitchtoDarkMode: React.FC<SwitchtoDarkModeProps> = ({ toggleTheme }) => {
-  if (typeof window === "undefined") {
-    return null;
-  }
-
   return (
     <div className="flex items-center">
       <input

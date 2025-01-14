6cc7bc431cd6b055caad8828a38e632cc0f497b4
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-14 07:39:45 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,35 @@
 "use client";
 
-import { Providers } from "./provider"; // Path to your provider
+import { Providers } from "./provider";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 import "./globals.css";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { setCookie, getCookie } from "@/lib/cookieUtils";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname(); // Get the current path
+
+  useEffect(() => {
+    // Save the current path as the last visited page
+    setCookie("lastPage", pathname);
+  }, [pathname]);
+
+  useEffect(() => {
+    // Redirect to the last visited page on load
+    const lastPage = getCookie("lastPage");
+    if (lastPage && pathname === "/") {
+      window.location.href = lastPage; // Redirect to the last visited page
+    }
+  }, []);
+
   return (
     <html lang="en">
-      <head>
-        {/* Initial theme-color */}
-        <meta name="theme-color" id="theme-color-meta" content="#ffffff" />
-      </head>
       <body>
         <Providers>
           <Nav />

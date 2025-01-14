60f661d55dab636b4cb0bd362af3f1610e6b370d
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-14 13:45:26 -0800

    Update cookieUtils.ts

--- lib/cookieUtils.ts
+++ lib/cookieUtils.ts
@@ -1,23 +1,13 @@
-export const setCookie = (
-  name: string,
-  value: string,
-  options: { path?: string; maxAge?: number } = {}
-) => {
-  let cookieString = `${name}=${value};`;
-
-  if (options.path) {
-    cookieString += `Path=${options.path};`;
+export const setCookie = (name: string, value: string, options?: { path?: string; maxAge?: number }) => {
+  let cookieString = `${name}=${value}; path=${options?.path || "/"};`;
+  if (options?.maxAge) {
+    cookieString += ` max-age=${options.maxAge};`;
   }
-
-  if (options.maxAge) {
-    cookieString += `Max-Age=${options.maxAge};`;
-  }
-
   document.cookie = cookieString;
 };
 
 export const getCookie = (name: string): string | null => {
   const cookies = document.cookie.split("; ");
-  const cookie = cookies.find((row) => row.startsWith(`${name}=`));
+  const cookie = cookies.find((c) => c.startsWith(`${name}=`));
   return cookie ? cookie.split("=")[1] : null;
 };

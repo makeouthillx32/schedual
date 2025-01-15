bebfcbaed9902fd3181d612e59f4ac426c057c26
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-15 09:11:10 -0800

    Create toolsConfig.ts

---
+++ lib/toolsConfig.ts
@@ -0,0 +1,14 @@
+export const tools = [
+  {
+    name: "Timesheet Calculator",
+    path: "/tools/timesheet-calculator",
+  },
+  {
+    name: "Tool 1",
+    path: "/tools/tool1",
+  },
+  {
+    name: "Tool 2",
+    path: "/tools/tool2",
+  },
+];

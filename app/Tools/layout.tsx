5f84bfedc6d0cfc597823b8d8d32bf36dc718a61
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-15 09:31:45 -0800

    Update layout.tsx

--- app/Tools/layout.tsx
+++ app/Tools/layout.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import { Providers } from "@/app/provider";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
-import "./globals.css";
+import "@/globals.css";
 
 interface LayoutProps {
   children: React.ReactNode;

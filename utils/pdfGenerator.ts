bdbf72a9f25ead7e061d65137f8b41fa6f339021
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-16 14:19:17 -0800

    Update pdfGenerator.ts

--- utils/pdfGenerator.ts
+++ utils/pdfGenerator.ts
@@ -1,27 +1,16 @@
 import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
 
-export async function generatePDF(selectedTemplate: string) {
-  const element = document.querySelector(".grid"); // Select punch card grid
-  if (!element || !(element instanceof HTMLElement)) {
-    console.error("Punch card grid not found or not a valid HTML element.");
-    return;
-  }
-
-  const canvas = await html2canvas(element, {
-    scale: 3, // Higher scale for better resolution
-    backgroundColor: null, // Preserve transparency
-  });
-
-  const imgData = canvas.toDataURL("image/png");
-
-  // Create a PDF with Letter Size (8.5" x 11")
+export async function generatePDF(compositePages: string[]) {
   const pdf = new jsPDF({
     orientation: "portrait",
     unit: "px",
-    format: [816, 1056], // Letter size in pixels
+    format: [2480, 3508], // Letter size
   });
 
-  pdf.addImage(imgData, "PNG", 20, 20, 776, 1016); // Adjust margins
-  pdf.save(`PunchCards-${selectedTemplate}.pdf`);
+  compositePages.forEach((page, index) => {
+    if (index !== 0) pdf.addPage();
+    pdf.addImage(page, "PNG", 0, 0, 2480, 3508);
+  });
+
+  pdf.save("PunchCards.pdf");
 }

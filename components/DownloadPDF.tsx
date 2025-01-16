d18c3ac57597958ce4addbf15e7e11e8fc278939
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-16 12:28:48 -0800

    Update DownloadPDF.tsx

--- components/DownloadPDF.tsx
+++ components/DownloadPDF.tsx
@@ -3,11 +3,15 @@
 import React from "react";
 import { generatePDF } from "@/utils/pdfGenerator";
 
-const DownloadPDF: React.FC = () => {
+interface DownloadPDFProps {
+  selectedTemplate: string;
+}
+
+const DownloadPDF: React.FC<DownloadPDFProps> = ({ selectedTemplate }) => {
   return (
     <button
       className="mt-6 p-3 bg-green-600 text-white rounded"
-      onClick={generatePDF}
+      onClick={() => generatePDF(selectedTemplate)} // Pass selectedTemplate explicitly
     >
       Download as PDF
     </button>

a288dc655092048835e8cab04ccab69510b9eae9
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-01-16 14:20:14 -0800

    Update DownloadPDF.tsx

--- components/DownloadPDF.tsx
+++ components/DownloadPDF.tsx
@@ -1,19 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { generateIndividualPunchCards } from "@/utils/generate_individual_punchcards";
+import { createCompositePages } from "@/utils/create_composite_image";
 import { generatePDF } from "@/utils/pdfGenerator";
 
 interface DownloadPDFProps {
   selectedTemplate: string;
+  numPunchCards: number;
 }
 
-const DownloadPDF: React.FC<DownloadPDFProps> = ({ selectedTemplate }) => {
+const DownloadPDF: React.FC<DownloadPDFProps> = ({
+  selectedTemplate,
+  numPunchCards,
+}) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleDownload = async () => {
+    setLoading(true);
+    const individualCards = await generateIndividualPunchCards(
+      selectedTemplate,
+      numPunchCards
+    );
+    const compositePages = await createCompositePages(individualCards);
+    await generatePDF(compositePages);
+    setLoading(false);
+  };
+
   return (
     <button
       className="mt-6 p-3 bg-green-600 text-white rounded"
-      onClick={() => generatePDF(selectedTemplate)} // Call generatePDF with correct template
+      onClick={handleDownload}
+      disabled={loading}
     >
-      Download as PDF
+      {loading ? "Generating..." : "Download as PDF"}
     </button>
   );
 };

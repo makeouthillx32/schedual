6e23ccd71613eb25511e941841824f4f61355778
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-02-13 12:32:53 -0800

    Update page.tsx

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,10 +1,12 @@
 import ProductCatalog from "./catalog";
-import { Providers } from "@/app/provider"; // ✅ Correct import
+import ProductManager from "./productManager";
+import { Providers } from "@/app/provider";
 
 export default function Page() {
   return (
     <Providers>
       <main className="p-6">
+        <ProductManager />
         <ProductCatalog />
       </main>
     </Providers>

9ee444adee92b8fe41d70f57164e9a6e3cf2b4a2
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-04-09 09:41:21 -0700

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
-  const redirectTo = url.searchParams.get("redirect_to") || "/protected";
+  const redirectTo = url.searchParams.get("redirect_to") || "/CMS";
 
   return NextResponse.redirect(new URL(redirectTo, process.env.NEXT_PUBLIC_SITE_URL));
 }

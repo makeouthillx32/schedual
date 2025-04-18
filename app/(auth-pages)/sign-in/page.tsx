834ffaaa58d9024d76b8adc27939ab69040b199d
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-04-18 08:49:50 -0700

    Update page.tsx

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,7 +1,19 @@
-"use client"; // ← ✅ Make this a client component
-
+"use client";
 import SignInForm from "@/components/SignInForm";
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function SignInPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams.get("refresh") === "true") {
+      router.refresh();
+      const newUrl = `${window.location.pathname}`;
+      window.history.replaceState({}, "", newUrl);
+    }
+  }, [searchParams, router]);
+
   return <SignInForm />;
 }

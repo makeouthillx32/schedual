19bd6e13398841d2278cc674d6bfb904a2180dc0
makeouthillx32 <skillet1005@gmail.com>
2025-04-21 11:47:42 -0700

    sas

--- components/ui/SignInWithGoogle.tsx
+++ components/ui/SignInWithGoogle.tsx
@@ -8,11 +8,16 @@ export default function SignInWithGoogle() {
   const router = useRouter();
 
   const handleGoogleSignIn = async () => {
+    const next = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("next="))
+      ?.split("=")[1];
+
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${location.origin}/auth/callback`
-      }
+        redirectTo: `${location.origin}/auth/callback${next ? `?next=${next}` : ""}`,
+      },
     });
 
     if (error) {

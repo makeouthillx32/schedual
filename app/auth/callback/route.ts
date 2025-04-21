9527f12c033964afa476e4e710903b27402c60e5
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-04-21 12:11:18 -0700

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,7 +1,16 @@
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+
+  await supabase.auth.getSession();
+
+  const lastPage = cookieStore.get("lastPage")?.value;
+
   return NextResponse.redirect(
-    new URL("/sign-in", process.env.NEXT_PUBLIC_SITE_URL || "https://schedual-five.vercel.app")
+    new URL(lastPage!, process.env.NEXT_PUBLIC_SITE_URL || "https://schedual-five.vercel.app")
   );
 }

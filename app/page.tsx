7c8e282c3998a428fbaa83e18a8cc4387bdb3b13
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-04-29 14:49:55 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,6 @@
 
 import Home from "@/components/home/Home";
 
-export const generateMetadata = () => ({
-  title: "Home | Desert Area Resources and Training",
-  description: "Welcome to Desert Area Resources and Training.",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#ffffff" }, // light mode for Home
-    { media: "(prefers-color-scheme: dark)", color: "#2d3142" },   // dark mode for Home
-  ],
-});
-
 export default function Page() {
   return (
     <div>

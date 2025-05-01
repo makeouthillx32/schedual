1f43d5824a31bcf5526214b7d5280bff417e30e6
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-01 09:51:41 -0700

    Create main.tsx

---
+++ components/home/GetInvolved/main.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import "@/app/globals.css";
+
+export default function GetInvolved() {
+  return (
+    <div className="min-h-screen bg-[var(--home-background)] text-[var(--home-text)] p-8">
+      <h1 className="text-3xl font-bold mb-4">Get Involved</h1>
+      <p className="text-lg">
+        There are a wide variety of ways you can get involved with Desert Area Resources and Training –
+        sponsoring events, volunteering your time, hosting a fundraiser, and more! 
+        <br /><br />
+        Desert Area Resources and Training is over 70% self-funded, and that means that every dollar 
+        earned at one of our signature events goes directly into the programs and services for over 
+        3,000 adults with disabilities we serve.
+      </p>
+    </div>
+  );
+}

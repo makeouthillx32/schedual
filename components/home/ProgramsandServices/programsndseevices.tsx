c169b389f2da19edb619d4dd10a8284965e270d8
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-01 08:28:26 -0700

    Update programsndseevices.tsx

--- components/home/ProgramsandServices/programsndseevices.tsx
+++ components/home/ProgramsandServices/programsndseevices.tsx
@@ -5,9 +5,9 @@ import "@/app/globals.css";
 
 export default function ProgramsAndServices() {
   return (
-    <div className="min-h-screen bg-[var(--home-background)] text-[var(--home-text)] p-8">
+    <section id="programs" className="min-h-screen bg-[var(--home-background)] text-[var(--home-text)] p-8">
       <h1 className="text-3xl font-bold mb-4">Programs & Services</h1>
       <p className="text-lg">Soon to be filled with content...</p>
-    </div>
+    </section>
   );
 }

9f7dc316a2b4142d9dd9a9b0a079cec446dec8fb
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-05 08:25:44 -0700

    Update Avatar.tsx

--- components/profile/Avatar.tsx
+++ components/profile/Avatar.tsx
@@ -9,7 +9,7 @@ export default function Avatar({ avatarUrl, userId, role }: {
   userId: string;
   role?: string;
 }) {
-  const fallbackUrl = "https://chsmesvozsjcgrwuimld.supabase.co/storage/v1/object/public/avatars/default.png";
+  const fallbackUrl = "https://chsmesvozsjcgrwuimld.supabase.co/storage/v1/object/public/avatars//Defult.png";
 
   return (
     <div className="relative flex flex-col items-center gap-4">

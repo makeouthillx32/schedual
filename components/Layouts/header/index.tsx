bfb1e3411fc7a0db5b0b72d53a694708ce41f945
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-06 08:49:11 -0700

    Update index.tsx

--- components/Layouts/header/index.tsx
+++ components/Layouts/header/index.tsx
@@ -11,14 +11,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { LogOutIcon, SettingsIcon, UserIcon } from "./icons";
-import { useProfileId } from "@/components/Layouts/sidebar/use-profile-id";
 import Avatar from "@/components/profile/Avatar";
-import { useSession } from "@/utils/supabase/client";
+import { useProfile } from "@/components/Layouts/sidebar/use-profile-id";
 
 export function UserInfo() {
   const [isOpen, setIsOpen] = useState(false);
-  const { profile, displayName } = useProfileId();
-  const { session } = useSession();
+  const { profile, displayName } = useProfile();
 
   return (
     <Dropdown isOpen={isOpen} setIsOpen={setIsOpen}>
@@ -26,15 +24,12 @@ export function UserInfo() {
         <span className="sr-only">My Account</span>
 
         <figure className="flex items-center gap-3">
-          <Avatar size={48} />
+          <Avatar className="size-12" />
           <figcaption className="flex items-center gap-1 font-medium text-dark dark:text-dark-6 max-[1024px]:sr-only">
             <span>{displayName}</span>
             <ChevronUpIcon
               aria-hidden
-              className={cn(
-                "rotate-180 transition-transform",
-                isOpen && "rotate-0"
-              )}
+              className={cn("rotate-180 transition-transform", isOpen && "rotate-0")}
               strokeWidth={1.5}
             />
           </figcaption>
@@ -48,7 +43,7 @@ export function UserInfo() {
         <h2 className="sr-only">User information</h2>
 
         <figure className="flex items-center gap-2.5 px-5 py-3.5">
-          <Avatar size={48} />
+          <Avatar className="size-12" />
           <figcaption className="space-y-1 text-base font-medium">
             <div className="mb-2 leading-none text-dark dark:text-white">
               {displayName}
@@ -61,7 +56,7 @@ export function UserInfo() {
 
         <div className="p-2 text-base text-[#4B5563] dark:text-dark-6 [&>*]:cursor-pointer">
           <Link
-            href={`/dashboard/${profile?.id}`}
+            href="/dashboard/me"
             onClick={() => setIsOpen(false)}
             className="flex w-full items-center gap-2.5 rounded-lg px-2.5 py-[9px] hover:bg-gray-2 hover:text-dark dark:hover:bg-dark-3 dark:hover:text-white"
           >
@@ -70,7 +65,7 @@ export function UserInfo() {
           </Link>
 
           <Link
-            href="/pages/settings"
+            href="/settings"
             onClick={() => setIsOpen(false)}
             className="flex w-full items-center gap-2.5 rounded-lg px-2.5 py-[9px] hover:bg-gray-2 hover:text-dark dark:hover:bg-dark-3 dark:hover:text-white"
           >

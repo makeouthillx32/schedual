945910db8defd678da3d58609faef8dd5c2a6e14
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-06 09:00:44 -0700

    Update index.tsx

--- components/Layouts/header/user-info/index.tsx
+++ components/Layouts/header/user-info/index.tsx
@@ -7,19 +7,15 @@ import {
   DropdownTrigger,
 } from "@/components/ui/dropdown";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { LogOutIcon, SettingsIcon, UserIcon } from "./icons";
+import { useProfile } from "@/components/Layouts/sidebar/use-profile-id";
+import Avatar from "@/components/profile/Avatar";
 
 export function UserInfo() {
   const [isOpen, setIsOpen] = useState(false);
-
-  const USER = {
-    name: "John Smith",
-    email: "johnson@nextadmin.com",
-    img: "/images/user/user-03.png",
-  };
+  const { profile, displayName } = useProfile();
 
   return (
     <Dropdown isOpen={isOpen} setIsOpen={setIsOpen}>
@@ -27,23 +23,12 @@ export function UserInfo() {
         <span className="sr-only">My Account</span>
 
         <figure className="flex items-center gap-3">
-          <Image
-            src={USER.img}
-            className="size-12"
-            alt={`Avatar of ${USER.name}`}
-            role="presentation"
-            width={200}
-            height={200}
-          />
+          <Avatar className="size-12" />
           <figcaption className="flex items-center gap-1 font-medium text-dark dark:text-dark-6 max-[1024px]:sr-only">
-            <span>{USER.name}</span>
-
+            <span>{displayName}</span>
             <ChevronUpIcon
               aria-hidden
-              className={cn(
-                "rotate-180 transition-transform",
-                isOpen && "rotate-0"
-              )}
+              className={cn("rotate-180 transition-transform", isOpen && "rotate-0")}
               strokeWidth={1.5}
             />
           </figcaption>
@@ -57,21 +42,10 @@ export function UserInfo() {
         <h2 className="sr-only">User information</h2>
 
         <figure className="flex items-center gap-2.5 px-5 py-3.5">
-          <Image
-            src={USER.img}
-            className="size-12"
-            alt={`Avatar for ${USER.name}`}
-            role="presentation"
-            width={200}
-            height={200}
-          />
-
+          <Avatar className="size-12" />
           <figcaption className="space-y-1 text-base font-medium">
-            <div className="mb-2 leading-none text-dark dark:text-white">
-              {USER.name}
-            </div>
-
-            <div className="leading-none text-gray-6">{USER.email}</div>
+            <div className="mb-2 leading-none text-dark dark:text-white">{displayName}</div>
+            <div className="leading-none text-gray-6">{profile?.email}</div>
           </figcaption>
         </figure>
 

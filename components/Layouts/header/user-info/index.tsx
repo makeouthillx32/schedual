2d204510138dfe638b3f434e4e0083cefef9fff8
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-06 09:05:32 -0700

    Update index.tsx

--- components/Layouts/header/user-info/index.tsx
+++ components/Layouts/header/user-info/index.tsx
@@ -10,12 +10,12 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { useState } from "react";
 import { LogOutIcon, SettingsIcon, UserIcon } from "./icons";
-import { useProfile } from "@/components/Layouts/sidebar/use-profile-id";
 import Avatar from "@/components/profile/Avatar";
+import { useProfileId } from "@/components/Layouts/sidebar/use-profile-id";
 
 export function UserInfo() {
   const [isOpen, setIsOpen] = useState(false);
-  const { profile, displayName } = useProfile();
+  const { profile, displayName } = useProfileId();
 
   return (
     <Dropdown isOpen={isOpen} setIsOpen={setIsOpen}>
@@ -23,12 +23,15 @@ export function UserInfo() {
         <span className="sr-only">My Account</span>
 
         <figure className="flex items-center gap-3">
-          <Avatar className="size-12" />
+          <Avatar userId={profile?.id} role={profile?.role} />
           <figcaption className="flex items-center gap-1 font-medium text-dark dark:text-dark-6 max-[1024px]:sr-only">
             <span>{displayName}</span>
             <ChevronUpIcon
               aria-hidden
-              className={cn("rotate-180 transition-transform", isOpen && "rotate-0")}
+              className={cn(
+                "rotate-180 transition-transform",
+                isOpen && "rotate-0"
+              )}
               strokeWidth={1.5}
             />
           </figcaption>
@@ -42,9 +45,11 @@ export function UserInfo() {
         <h2 className="sr-only">User information</h2>
 
         <figure className="flex items-center gap-2.5 px-5 py-3.5">
-          <Avatar className="size-12" />
+          <Avatar userId={profile?.id} role={profile?.role} />
           <figcaption className="space-y-1 text-base font-medium">
-            <div className="mb-2 leading-none text-dark dark:text-white">{displayName}</div>
+            <div className="mb-2 leading-none text-dark dark:text-white">
+              {displayName}
+            </div>
             <div className="leading-none text-gray-6">{profile?.email}</div>
           </figcaption>
         </figure>

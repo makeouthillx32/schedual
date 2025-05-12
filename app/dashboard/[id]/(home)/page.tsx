a375e1de8e503edc0f2065a97c73c23a9a2533e7
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-12 11:17:19 -0700

    Update page.tsx

--- app/dashboard/[id]/(home)/page.tsx
+++ app/dashboard/[id]/(home)/page.tsx
@@ -19,3 +19,44 @@ type PropsType = {
 export default async function Home({ searchParams }: PropsType) {
   const { selected_time_frame } = await searchParams;
   const extractTimeFrame = createTimeFrameExtractor(selected_time_frame);
+
+  return (
+    <>
+      <Suspense fallback={<OverviewCardsSkeleton />}>
+        <OverviewCardsGroup />
+      </Suspense>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
+        <PaymentsOverview
+          className="col-span-12 xl:col-span-7"
+          key={extractTimeFrame("payments_overview")}
+          timeFrame={extractTimeFrame("payments_overview")?.split(":")[1]}
+        />
+
+        <WeeksProfit
+          key={extractTimeFrame("weeks_profit")}
+          timeFrame={extractTimeFrame("weeks_profit")?.split(":")[1]}
+          className="col-span-12 xl:col-span-5"
+        />
+
+        <UsedDevices
+          className="col-span-12 xl:col-span-5"
+          key={extractTimeFrame("used_devices")}
+          timeFrame={extractTimeFrame("used_devices")?.split(":")[1]}
+        />
+
+        <RegionLabels />
+
+        <div className="col-span-12 grid xl:col-span-8">
+          <Suspense fallback={<TopChannelsSkeleton />}>
+            <TopChannels />
+          </Suspense>
+        </div>
+
+        <Suspense fallback={null}>
+          <ChatsCard />
+        </Suspense>
+      </div>
+    </>
+  );
+}

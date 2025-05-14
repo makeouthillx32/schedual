8ccf0e20ecef3b3746550e22c9dc65deef9a9ba9
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-14 13:44:44 -0700

    Update page.tsx

--- app/dashboard/[id]/settings/page.tsx
+++ app/dashboard/[id]/settings/page.tsx
@@ -2,12 +2,17 @@ import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import type { Metadata } from "next";
 import { PersonalInfoForm } from "./_components/personal-info";
 import { UploadPhotoForm } from "./_components/upload-photo";
+import ManualRoleEditor from "@/components/profile/ManualRoleEditor";
+import AdminUserManager from "@/components/profile/AdminDelete";
+import { getUserProfile } from "@/lib/getUserProfile";
 
 export const metadata: Metadata = {
   title: "Settings Page",
 };
 
-export default function SettingsPage() {
+export default async function SettingsPage() {
+  const profile = await getUserProfile();
+
   return (
     <div className="mx-auto w-full max-w-[1080px]">
       <Breadcrumb pageName="Settings" />
@@ -20,7 +25,13 @@ export default function SettingsPage() {
           <UploadPhotoForm />
         </div>
       </div>
+
+      {profile?.role === "admin" && (
+        <div className="mt-12 space-y-12">
+          <ManualRoleEditor />
+          <AdminUserManager />
+        </div>
+      )}
     </div>
   );
-};
-
+}

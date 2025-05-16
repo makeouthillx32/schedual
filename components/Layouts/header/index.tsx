a3d366abee91fa16f3044508a5823711ce8880fe
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-16 13:55:22 -0700

    Update index.tsx

--- components/Layouts/header/index.tsx
+++ components/Layouts/header/index.tsx
@@ -53,8 +53,10 @@ export function Header() {
           <SearchIcon className="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2 max-[1015px]:size-5" />
         </div>
 
-        {/* Conditionally render theme toggle */}
-        {isMobile ? <SwitchtoDarkMode /> : <ThemeToggleSwitch />}
+        {/* Theme toggle with transparent border for diagnostics */}
+        <div className="theme-toggle-container border-2 border-transparent flex items-center justify-center">
+          {isMobile ? <SwitchtoDarkMode /> : <ThemeToggleSwitch />}
+        </div>
 
         {/* This will show the notification bell with dot */}
         <Notification />

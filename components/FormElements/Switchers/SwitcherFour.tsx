7fdac427f7ed2437bec61c29220df13034ede9bf
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-20 14:26:36 -0700

    Update SwitcherFour.tsx

--- components/FormElements/Switchers/SwitcherFour.tsx
+++ components/FormElements/Switchers/SwitcherFour.tsx
@@ -18,9 +18,9 @@ const SwitcherFour = () => {
               setEnabled(!enabled);
             }}
           />
-          <div className="block h-8 w-14 rounded-full bg-[#212B36] dark:bg-primary"></div>
+          <div className="block h-8 w-14 rounded-full bg-[hsl(var(--foreground))] dark:bg-[hsl(var(--sidebar-primary))]"></div>
           <div
-            className={`absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-white transition ${
+            className={`absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-[hsl(var(--background))] transition ${
               enabled && "!right-1 !translate-x-full"
             }`}
           ></div>

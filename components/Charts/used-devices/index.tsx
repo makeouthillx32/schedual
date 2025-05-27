17c9c113ceb146995b474844b12c7afbef1102a3
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-27 13:47:55 -0700

    Update index.tsx

--- components/Charts/used-devices/index.tsx
+++ components/Charts/used-devices/index.tsx
@@ -1,6 +1,6 @@
 import { PeriodPicker } from "@/components/period-picker";
 import { cn } from "@/lib/utils";
-import { getDevicesUsedData } from "@/services/device.services";
+import { getDevicesUsedData } from "@/services/device.service";
 import { DonutChart } from "./chart";
 
 type PropsType = {

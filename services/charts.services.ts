2c76f4fd2413bb113be8131d63fe88618d4de3c1
makeouthillx32 <skillet1005@gmail.com>
2025-05-29 12:04:55 -0700

    adssdasd

--- services/charts.services.ts
+++
@@ -1,7 +0,0 @@
-// Re-export all chart services from their individual files
-export { getDevicesUsedData } from './devices.service';
-export { getPaymentsOverviewData } from './payments.service';
-export { getWeeksProfitData } from './profits.service';
-export { getCampaignVisitorsData } from './campaigns.service';
-export { getVisitorsAnalyticsData } from './visitors.service';
-export { getCostsPerInteractionData } from './costs.service';

dbee7516084cc401b1e604c4516b34a3e0a8bdd9
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-05-30 10:04:58 -0700

    Update icons.tsx

--- app/dashboard/[id]/(home)/_components/overview-cards/icons.tsx
+++ app/dashboard/[id]/(home)/_components/overview-cards/icons.tsx
@@ -5,7 +5,7 @@ type SVGPropsType = SVGProps<SVGSVGElement>;
 export function Views(props: SVGPropsType) {
   return (
     <svg width={58} height={58} viewBox="0 0 58 58" fill="none" {...props}>
-      <circle cx={29} cy={29} r={29} fill="#3FD97F" />
+      <circle cx={29} cy={29} r={29} fill="hsl(var(--chart-1))" />
       <path
         d="M26.562 29a2.437 2.437 0 114.875 0 2.437 2.437 0 01-4.875 0z"
         fill="#fff"
@@ -23,7 +23,7 @@ export function Views(props: SVGPropsType) {
 export function Profit(props: SVGPropsType) {
   return (
     <svg width={58} height={58} viewBox="0 0 58 58" fill="none" {...props}>
-      <circle cx={29} cy={29} r={29} fill="#FF9C55" />
+      <circle cx={29} cy={29} r={29} fill="hsl(var(--chart-2))" />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
@@ -37,7 +37,7 @@ export function Profit(props: SVGPropsType) {
 export function Product(props: SVGPropsType) {
   return (
     <svg width={58} height={58} viewBox="0 0 58 58" fill="none" {...props}>
-      <circle cx={29} cy={29} r={29} fill="#8155FF" />
+      <circle cx={29} cy={29} r={29} fill="hsl(var(--chart-3))" />
       <path
         d="M35.043 20.8l-2.167-1.136c-1.902-.998-2.853-1.498-3.876-1.498-1.023 0-1.974.5-3.876 1.498L22.958 20.8c-1.922 1.008-3.051 1.6-3.752 2.394L29 28.09l9.794-4.896c-.7-.793-1.83-1.386-3.751-2.394zM39.56 24.628l-9.747 4.874v10.227c.777-.194 1.662-.658 3.063-1.393l2.167-1.137c2.33-1.223 3.496-1.835 4.143-2.934.647-1.099.647-2.467.647-5.202v-.127c0-2.05 0-3.332-.272-4.308zM28.188 39.73V29.501l-9.749-4.874c-.272.976-.272 2.258-.272 4.308v.127c0 2.735 0 4.103.647 5.202.647 1.1 1.813 1.71 4.144 2.934l2.166 1.137c1.4.735 2.286 1.2 3.064 1.393z"
         fill="#fff"
@@ -49,7 +49,7 @@ export function Product(props: SVGPropsType) {
 export function Users(props: SVGPropsType) {
   return (
     <svg width={58} height={58} viewBox="0 0 58 58" fill="none" {...props}>
-      <circle cx={29} cy={29} r={29} fill="#18BFFF" />
+      <circle cx={29} cy={29} r={29} fill="hsl(var(--chart-4))" />
       <ellipse
         cx={25.7511}
         cy={22.4998}

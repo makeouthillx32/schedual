325a5d8ce3ac7874ca00824a11d935b6256a8ad3
makeouthillx32 <skillet1005@gmail.com>
2025-06-04 11:21:51 -0700

    ASDAS

---
+++ app/dashboard/[id]/(home)/admin/_components/overview-cards/index.tsx
@@ -0,0 +1,57 @@
+import { compactFormat } from "@/lib/format-number";
+import { getOverviewData } from "../../fetch";
+import { OverviewCard } from "./card";
+import * as icons from "./icons";
+
+export async function OverviewCardsGroup() {
+  const { views, profit, products, members, users } = await getOverviewData();
+
+  return (
+    <div className="grid gap-4 sm:grid-cols-2 sm:gap-6 xl:grid-cols-5 2xl:gap-7.5">
+      <OverviewCard
+        label="Total Views"
+        data={{
+          ...views,
+          value: compactFormat(views.value),
+        }}
+        Icon={icons.Views}
+      />
+
+      <OverviewCard
+        label="Total Profit"
+        data={{
+          ...profit,
+          value: "$" + compactFormat(profit.value),
+        }}
+        Icon={icons.Profit}
+      />
+
+      <OverviewCard
+        label="Total Products"
+        data={{
+          ...products,
+          value: compactFormat(products.value),
+        }}
+        Icon={icons.Product}
+      />
+
+      <OverviewCard
+        label="Total Members"
+        data={{
+          ...members,
+          value: compactFormat(members.value),
+        }}
+        Icon={icons.Users}
+      />
+
+      <OverviewCard
+        label="Total Users"
+        data={{
+          ...users,
+          value: compactFormat(users.value),
+        }}
+        Icon={icons.Users}
+      />
+    </div>
+  );
+}

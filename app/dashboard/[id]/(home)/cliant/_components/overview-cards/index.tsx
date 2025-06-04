7cf101a9ac746c3e459c8a0e4a75556650ee21a2
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-06-04 14:16:28 -0700

    Update index.tsx

--- app/dashboard/[id]/(home)/cliant/_components/overview-cards/index.tsx
+++ app/dashboard/[id]/(home)/cliant/_components/overview-cards/index.tsx
@@ -1,56 +1,70 @@
+
 import { compactFormat } from "@/lib/format-number";
-import { getOverviewData } from "../../fetch";
 import { OverviewCard } from "./card";
 import * as icons from "./icons";
 
 export async function OverviewCardsGroup() {
-  const { views, profit, products, members, users } = await getOverviewData();
+  // Dummy data for client dashboard
+  const goal1 = {
+    value: 85,
+    percentage: 12.5,
+    isPositive: true
+  };
+
+  const goal2 = {
+    value: 92,
+    percentage: 8.3,
+    isPositive: true
+  };
+
+  const goal3 = {
+    value: 78,
+    percentage: -3.2,
+    isPositive: false
+  };
+
+  const nextSls = {
+    value: "June 15",
+    percentage: 0,
+    isPositive: true
+  };
 
   return (
-    <div className="grid gap-4 sm:grid-cols-2 sm:gap-6 xl:grid-cols-5 2xl:gap-7.5">
+    <div className="grid gap-4 sm:grid-cols-2 sm:gap-6 xl:grid-cols-4 2xl:gap-7.5">
       <OverviewCard
-        label="Total Views"
+        label="Goal 1"
         data={{
-          ...views,
-          value: compactFormat(views.value),
-        }}
-        Icon={icons.Views}
-      />
-
-      <OverviewCard
-        label="Total Profit"
-        data={{
-          ...profit,
-          value: "$" + compactFormat(profit.value),
-        }}
-        Icon={icons.Profit}
-      />
-
-      <OverviewCard
-        label="Total Products"
-        data={{
-          ...products,
-          value: compactFormat(products.value),
+          ...goal1,
+          value: goal1.value + "%",
         }}
         Icon={icons.Product}
       />
 
       <OverviewCard
-        label="Total Members"
+        label="Goal 2"
         data={{
-          ...members,
-          value: compactFormat(members.value),
+          ...goal2,
+          value: goal2.value + "%",
         }}
-        Icon={icons.Users}
+        Icon={icons.Product}
       />
 
       <OverviewCard
-        label="Total Users"
+        label="Goal 3"
         data={{
-          ...users,
-          value: compactFormat(users.value),
+          ...goal3,
+          value: goal3.value + "%",
         }}
-        Icon={icons.Users}
+        Icon={icons.Product}
+      />
+
+      <OverviewCard
+        label="Next SLS Date"
+        data={{
+          ...nextSls,
+          value: nextSls.value,
+        }}
+        Icon={icons.Views}
       />
     </div>
   );

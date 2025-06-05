6aeb8bf876f4162e4f50fa9a94a89f6926c8618a
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-06-05 09:41:32 -0700

    Update page.tsx

--- app/dashboard/[id]/(home)/cliant/page.tsx
+++ app/dashboard/[id]/(home)/cliant/page.tsx
@@ -1,4 +1,6 @@
-import { ClientsList, ClientsListSkeleton } from "@/components/Tables/clients-list";
+
+import { MoodCheckIn, MoodCheckInSkeleton } from "./_components/mood-check";
+import { PersonalWins, PersonalWinsSkeleton } from "./_components/personal-wins";
 import { Suspense } from "react";
 import { ChatsCard } from "./_components/chats-card";
 import { OverviewCardsGroup } from "./_components/overview-cards";
@@ -18,9 +20,15 @@ export default async function Home({ searchParams }: PropsType) {
       </Suspense>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
-        <div className="col-span-12 xl:col-span-8">
-          <Suspense fallback={<ClientsListSkeleton />}>
-            <ClientsList />
+        <div className="col-span-12 xl:col-span-6">
+          <Suspense fallback={<PersonalWinsSkeleton />}>
+            <PersonalWins />
+          </Suspense>
+        </div>
+
+        <div className="col-span-12 xl:col-span-6">
+          <Suspense fallback={<MoodCheckInSkeleton />}>
+            <MoodCheckIn />
           </Suspense>
         </div>
 

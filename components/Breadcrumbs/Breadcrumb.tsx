eb4b505f046e1b717da510fb8f06f02a8606c9bf
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-07-16 06:18:33 -0700

    Update Breadcrumb.tsx

--- components/Breadcrumbs/Breadcrumb.tsx
+++ components/Breadcrumbs/Breadcrumb.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 interface BreadcrumbProps {
   pageName: string;
 }
 
 const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
+  const { id } = useParams() as { id: string };
+  const dashboardUrl = `/dashboard/${id}`;
+
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-[26px] font-bold leading-[30px] text-dark dark:text-white">
@@ -14,7 +20,7 @@ const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
       <nav>
         <ol className="flex items-center gap-2">
           <li>
-            <Link className="font-medium" href="/">
+            <Link className="font-medium" href={dashboardUrl}>
               Dashboard /
             </Link>
           </li>

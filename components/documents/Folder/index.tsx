28975292214f7b995fe903a7d31cb3b80efff624
unenter <63877528+makeouthillx32@users.noreply.github.com>
2025-07-18 08:19:19 -0700

    Update index.tsx

--- components/documents/Folder/index.tsx
+++ components/documents/Folder/index.tsx
@@ -3,7 +3,7 @@
 
 import React, { useState } from 'react';
 import { FolderIcon, StarIcon, StarFilledIcon, MoreVerticalIcon } from './icons';
-import './styles.css';
+import './style.css';
 
 interface FolderProps {
   folder: {
